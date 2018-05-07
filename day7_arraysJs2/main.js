989a37937dd7eb1e94515e1526236f1aa5fe38a6
jusin6302971 <jusin6302971@hotmail.com>
2018-05-07 23:31:14 +0800

    day7

---
+++ day7_arraysJs2/main.js
@@ -0,0 +1,44 @@
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
+var haveAdults=people.some(function(d){return new Date().getFullYear()-d.year>=19})
+console.log({haveAdults})
+var allAdults=people.every(function(d){return new Date().getFullYear()-d.year>=19})
+console.log({allAdults})
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+var result =comments.find(function(comment){
+  if(comment.id===823423){
+    return true;
+  }
+})
+
+console.log({result})
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+var index =comments.findIndex(function(comment){
+  if(comment.id===823423){
+    return true;
+  }
+})
+
+const newComment=[...comments.slice(0,index),...comments.slice(index+1)]
+console.log(newComment)
